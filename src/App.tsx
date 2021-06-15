9c4efd19800884e234f94dc1d4cc7e4e17b22525
malikarslan-94 <arslan.malik31@gmail.com>
2021-06-15 12:03:13 +0500

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  interface Provider {
+    name: string;
+    status: string;
+  }
+
+  const [taskname, setTaskname] = useState<string>("");
+  const [status, setStatus] = useState<string>("done");
+
+  const [task, setTask] = useState<Provider[]>([]);
+
+  const addTask = () => {
+    setTask((task) => [...task, { name: taskname, status: "none" }]);
+  };
+
+  const deleteTask = (tasks: string) => {
+    const result = task.filter((task) => task.name !== tasks);
+    setTask(result);
+  };
+  const inputTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskname(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do App</h1>
+      <ol style={{ marginTop: 30, display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex" }}>
+          <input placeholder="Enter task" onChange={inputTask}></input>
+          <button onClick={addTask}>Add task</button>
+        </div>
+      </ol>
+
+      <ol>
+        {task.map((item, index) => {
+          return (
+            <li
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 10
+              }}
+            >
+              <p>{item.name}</p>
+              <button onClick={() => deleteTask(item.name)}>Delete Task</button>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
 
-export default App;
